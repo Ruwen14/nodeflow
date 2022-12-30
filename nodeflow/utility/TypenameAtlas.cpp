59afa38c0e2c38f2b1ff6ece508cceb72389d33a
Ruwen14 <73891246+Ruwen14@users.noreply.github.com>
2022-12-30 17:18:00 +0100

    TypenameAtlas: Singleton structure to track reflected type names

---
+++ nodeflow/utility/TypenameAtlas.cpp
@@ -0,0 +1,36 @@
+#include "utility/TypenameAtlas.hpp"
+
+#include <algorithm>
+#include <iterator>
+#include <type_traits>
+
+
+namespace nf
+{
+
+	std::set<std::string> TypenameAtlas::listTypenames() const
+	{
+		std::set<std::string> ret;
+		std::transform(m_atlas.begin(), m_atlas.end(),
+			std::inserter(ret, ret.end()),
+			[](auto pair) { return pair.second; });
+
+		return ret;
+	}
+
+	bool TypenameAtlas::hasDuplicates() const
+	{
+		std::vector<std::string> atlasValues;
+		for (const auto& atla : m_atlas)
+			atlasValues.push_back(atla.second);
+
+		if (!std::is_sorted(atlasValues.begin(), atlasValues.end()))
+			std::sort(atlasValues.begin(), atlasValues.end());
+
+		return std::adjacent_find(atlasValues.begin(), atlasValues.end()) != atlasValues.end();
+	}
+
+}
+
+
+
---
+++ nodeflow/utility/TypenameAtlas.hpp
@@ -0,0 +1,91 @@
+/*
+- nodeflow -
+BSD 3-Clause License
+
+Copyright (c) 2022, Ruwen Kohm
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#pragma once
+
+#include <unordered_map>
+#include <set>
+#include <utility>
+#include <vector>
+
+#include "utility/Singleton.hpp"
+#include "typedefs.hpp"
+#include "reflection/type_reflection.hpp"
+
+namespace nf
+{
+	class TypenameAtlas : public Singleton<TypenameAtlas>
+	{
+	public:
+		using atlas_t = std::unordered_map<typeid_t, std::string>;
+
+	public:
+		TypenameAtlas() = default;
+
+		template<typename Type>
+		bool add()
+		{
+			auto id = type_id<Type>();
+			auto typeName = type_name<Type>();
+			if (m_atlas.contains(id))
+			{
+				NF_ASSERT(m_atlas.at(id) == typeName, "Hash collision of function: type_name<T>() detected. Please fix affected type name");
+				return false;
+			}
+			m_atlas[id] = typeName;
+			return true;
+		}
+
+		template<typename Type>
+		inline const std::string& get() const
+		{
+			return m_atlas.at(type_id<Type>());
+		}
+
+		inline const std::string& get(typeid_t id) const
+		{
+			return m_atlas.at(id);
+		}
+
+		inline bool contains(typeid_t id) const { return m_atlas.contains(id); }
+
+		inline const atlas_t& atlas() const { return m_atlas; }
+
+		std::set<std::string> listTypenames() const;
+		
+		bool hasDuplicates() const;
+
+	private:
+		atlas_t m_atlas;
+	};
+}
