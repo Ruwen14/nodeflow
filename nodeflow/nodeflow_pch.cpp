1fcd91ba779f8d146b5a19179ccf43dfa059b29c
Ruwen14 <ruwen.kohm@web.de>
2023-09-06 00:37:34 +0200

    Added precompiled headers for nodeflow core library

---
+++ nodeflow/nodeflow_pch.cpp
@@ -0,0 +1,1 @@
+#include "nodeflow_pch.h"
---
+++ nodeflow/nodeflow_pch.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+#include <unordered_map>
+
+#include "nodeflow/utility/dbgln.hpp"
+#include "nodeflow/reflection/type_reflection.hpp"
