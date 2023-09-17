6a88b8dd96f235de46e303288500bf7bd3dac224
Ruwen14 <ruwen.kohm@web.de>
2023-09-17 19:26:43 +0200

    Added Logging via spdlog

--- nodeflow/nodeflow_pch.h
+++ nodeflow/nodeflow_pch.h
@@ -8,6 +8,10 @@
 #include <utility>
 #include <vector>
 
+// #define SPDLOG_COMPILED_LIB
+// #include <spdlog/spdlog.h>
+
 #include "nodeflow/reflection/type_reflection.hpp"
 #include "nodeflow/utility/Expected.hpp"
+#include "nodeflow/utility/Logger.hpp"
 #include "nodeflow/utility/dbgln.hpp"
---
+++ nodeflow/utility/Logger.hpp
@@ -0,0 +1,42 @@
+/*
+- nodeflow -
+BSD 3-Clause License
+
+Copyright (c) 2022, Ruwen Kohm
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#pragma once
+#define SPDLOG_COMPILED_LIB
+#include <spdlog/spdlog.h>
+
+#define NF_TRACE(...) spdlog::trace(__VA_ARGS__)
+#define NF_INFO(...) spdlog::info(__VA_ARGS__)
+#define NF_WARN(...) spdlog::warn(__VA_ARGS__)
+#define NF_ERROR(...) spdlog::error(__VA_ARGS__)
+#define NF_CRITICAL(...) spdlog::critical(__VA_ARGS__)
